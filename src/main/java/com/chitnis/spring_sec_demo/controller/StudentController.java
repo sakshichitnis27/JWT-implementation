6077caa2d5f47b83b405e76d59a6a79abf0b2e2a
sakshichitnis27 <sakshigchitnis27@gmail.com>
2024-12-30 12:13:13 +0530

    commit

---
+++ src/main/java/com/chitnis/spring_sec_demo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.chitnis.spring_sec_demo.controller;
+
+
+import com.chitnis.spring_sec_demo.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1,"Sakshi","Java"),
+            new Student(2,"Akshat","Java")
+    ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student) {
+        students.add(student);
+    }
+
+}
